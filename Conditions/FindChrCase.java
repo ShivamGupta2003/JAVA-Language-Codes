532976b97797a2e44d1903a050a57a855db7085b
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-10-31 19:45:42 +0530

    Create FindChrCase.java

---
+++ Conditions/FindChrCase.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+           if(65<=c && c<=91)
+           {int i=1;
+            System.out.println(i);
+            }
+          else if(97<=c && c<=123)
+          { int j=0;
+              System.out.println(j);}
+              else 
+              {int k=-1;
+                  System.out.println(k);}
+    }
+}
