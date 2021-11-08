7d2edfab184813026cda2aa2cb9a97d92080c9dd
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-11-08 14:32:07 +0530

    Create ReturnArraySum.java

---
+++ Arrays/ReturnArraySum.java
@@ -0,0 +1,12 @@
+
+public class Solution {
+	
+	public static int sum(int[] arr) {
+		int i=0;
+            for(int j=0;j<arr.length;j++)
+            {
+                i=i+arr[j];
+            }
+        return i;
+	}
+}
