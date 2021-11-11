3ecb1b5a5cc8229b71bba395d33a17ad0f99af75
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-11-11 17:17:22 +0530

    Create BinaryToDecimal.java

---
+++ For Loops/BinaryToDecimal.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int power(int n,int x){
+        int a=n;
+        int j=1;
+       
+        {while(j<=x-1)
+        {
+           a=a*n;
+           j=j+1;
+        }
+    }
+         if (n!=0 && x!=0)
+        return a;
+        
+        else
+            return 1;
+    }
+     
+    public static void main(String[] args) {    
+    Scanner s = new Scanner(System.in);
+	int n = s.nextInt();
+      
+ 
+        int a=0;
+ int b=0;
+        int l=0;
+    for(int i=n;i>0;i=i/10)
+    {
+    a=i%10;
+    b=b+a*(power(2,l));
+        l++;
+    }
+          System.out.println(b);
+
+      }
+    
+ 
+
+
+       
+	}
+
