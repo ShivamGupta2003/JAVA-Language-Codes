03755df780d9a4eac26863e8f7ac4e822811f06a
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-11-14 15:01:41 +0530

    Create CheckPermutation.java

---
+++ Strings/CheckPermutation.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+	public static boolean isPermutation(String str1, String str2) {
+        
+        if(str1.length()!=str2.length())
+            return false;
+        
+        int fre[] = new int[256];
+        
+      //  for(int i=0;i<256;i++){
+       //     fre[i]=0;
+     //   }
+        
+        for(int i=0;i<str1.length();i++){
+            char ch = str1.charAt(i);
+            ++fre[ch];
+        }
+        
+        
+        for(int i=0;i<str2.length();i++){
+            char ch = str2.charAt(i);
+            --fre[ch];
+        }
+        
+        for(int i=0;i<256;i++){
+            if(fre[i]!=0){
+                return false;
+            }
+        }
