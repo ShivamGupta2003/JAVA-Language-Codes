30e3cba8f59c6f5aaf751284638da10dd71ba7d8
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-11-15 13:07:51 +0530

    Create AverageMarks.java

---
+++ Conditions/AverageMarks.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Solution {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+String str = s.next();
+    System.out.println(str);
+    int i=s.nextInt();
+    int j=s.nextInt();
+    int k=s.nextInt();
+    int l=(i+j+k)/3;
+    System.out.println(l);
+	}
+
+}
