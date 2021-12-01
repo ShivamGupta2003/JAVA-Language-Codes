b551c892b1d2f1b464f15c18d159af4e1a2f2b69
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-01 13:29:13 +0530

    Update Staircase.java

--- Recursion/Staircase.java
+++ Recursion/Staircase.java
@@ -1,3 +1,8 @@
+/*A child is running up a staircase with N steps, and can hop either 1 step, 2 steps or 3 steps at a time. 
+Implement a method to count how many possible ways the child can run up to the stairs. 
+You need to return number of possible ways W.*/
+
+// FUNCTIONAL CODE IS AS FOLLOWS:-
 public class Solution {	
      public static int staircase(int n){
 	
@@ -15,5 +20,16 @@ public class Solution {
          int sa3=staircase(n-3);
              return sa3+sa2+sa1;
       
+	}
+}
+
+// RUNNER CODE IS AS FOLLOWS:-
+import java.util.Scanner;
+
+public class Runner {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		System.out.println(Solution.staircase(n));
 	}
 }
