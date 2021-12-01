10231922cd1961e8931c16323d4d54a731425ad4
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-01 13:30:28 +0530

    Update CheckAB.java

--- Recursion/CheckAB.java
+++ Recursion/CheckAB.java
@@ -1,3 +1,9 @@
+/*Suppose you have a string, S, made up of only 'a's and 'b's. Write a recursive function that checks if the string was generated using the following rules:-
+a. The string begins with an 'a'
+b. Each 'a' is followed by nothing or an 'a' or "bb"
+c. Each "bb" is followed by nothing or an 'a'
+If all the rules are followed by the given string, return true otherwise return false.*/
+
 //functional code
 public class Solution {
 
@@ -29,7 +35,7 @@ public class Solution {
 
 
 
-//main code
+//RUNNER code
 import java.util.Scanner;
 
 public class Main {
