052d592bab0852dfe7be015a7a647aa9db53af37
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-02 17:38:13 +0530

    Create SumOfDigits.java

---
+++ Recursion/SumOfDigits.java
@@ -0,0 +1,25 @@
+//Write a recursive function that returns the sum of the digits of a given integer.
+  
+// FUNCTIONAL CODE
+public class solution {
+
+	public static int sumOfDigits(int input){
+	
+        if(input<=0)
+            return 0;
+        
+        else
+            return (input%10)+sumOfDigits(input/10);
+	}
+}
+
+// MAIN CODE
+import java.util.Scanner;
+
+public class runner {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		System.out.println(solution.sumOfDigits(n));
+	}
+}
