fbe148261917ebb6da09a1b6a52e7a7b0c9241a6
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-06 12:47:54 +0530

    Update PrintSubsetOfArray.java

--- Recursion/PrintSubsetOfArray.java
+++ Recursion/PrintSubsetOfArray.java
@@ -92,3 +92,61 @@ public class runner {
 
 
 
+// other way of doing functional code
+public class solution {
+	
+   
+    
+    
+ public static int[][] subsets(int input[]) {
+		// Write your code here
+ return subsets(input, 0);
+
+    }
+
+    public static int[][] subsets(int input[], int start) {
+        // Write your code here
+        if (start == input.length) {
+            int[][] myans = new int[1][0];
+            return myans;
+        }
+        
+        int[][] smallanswer = subsets(input, start + 1);
+        int[][] myans = new int[2 * smallanswer.length][];
+        int k = 0;
+
+        for (int i = 0; i < smallanswer.length; i++) {
+            myans[k] = new int[smallanswer[i].length];
+            for (int j = 0; j < smallanswer[i].length; j++) {
+                myans[k][j] = smallanswer[i][j];
+
+            }
+            k++;
+        }
+        for (int i = 0; i < smallanswer.length; i++) {
+            myans[k] = new int[smallanswer[i].length + 1];
+            myans[k][0]=input[start];
+            for (int j = 1; j <= smallanswer[i].length; j++) {
+                myans[k][j] = smallanswer[i][j - 1];
+            }
+            k++;
+        }
+
+        return myans;
+	}
+    
+    public static void printSubsets(int input[]) {    
+		int output[][] = subsets(input);
+		for(int i = 0; i < output.length; i++) {
+			for(int j = 0; j < output[i].length; j++) {
+				System.out.print(output[i][j] + " ");
+			}
+			System.out.println();
+        }
+	}
+    
+    
+    
+    
+    
+}
