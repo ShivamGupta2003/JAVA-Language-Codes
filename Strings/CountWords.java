131938fb01574fbd77de1745747167af6bd51f9f
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-08 14:45:14 +0530

    Update CountWords.java

--- Strings/CountWords.java
+++ Strings/CountWords.java
@@ -1,3 +1,10 @@
+/*For a given input string(str), write a function to print all the possible substrings.
+	Substring
+A substring is a contiguous sequence of characters within a string. 
+Example: "cod" is a substring of "coding". Whereas, "cdng" is not as the characters taken are not contiguous*/
+
+
+// FUNCTIONAL CODE
 public class Solution {
 
 	public static int countWords(String str) {	
@@ -18,4 +25,28 @@ public class Solution {
         return a;
 	}
 
+}
+
+
+// MAIN CODE
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Runner {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		String str = br.readLine();
+		if (str != null) {
+			str = str.trim();
+		} else {
+			str = "";
+		}
+
+		Solution.printSubstrings(str);
+		
+	}
 }
