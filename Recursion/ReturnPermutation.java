4d60e0745497cac2bfcc6c703d5dae946d68d4c7
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-09 13:24:37 +0530

    Create ReturnPermutation.java

---
+++ Recursion/ReturnPermutation.java
@@ -0,0 +1,45 @@
+/*Given a string S, find and return all the possible permutations of the input string.
+Note 1 : The order of permutations is not important.
+Note 2 : If original string contains duplicate characters, permutations will also be duplicates.*/
+
+// FUNCTIONAL CODE
+public class solution {
+	
+  public static int factorial(int n)
+  {
+      int ans=1;
+          
+      for(int i=1;i<=n;i++)
+              ans=ans*i;
+      
+      return ans;
+  }
+    
+	public static String[] permutationOfString(String input){
+		
+        if(input.length()==0)
+        {
+            String ans[]=new String[1];
+            ans[0]="";
+            return ans;
+        }
+        
+        String ans[]=new String[factorial(input.length())];
+        int k=0;
+        
+        for(int i=0;i<input.length();i++)
+        {
+            String recursiveInput=input.substring(0,i)+input.substring(i+1,input.length());
+            String smallAns[]=permutationOfString(recursiveInput);
+            
+            for(int j=0;j<smallAns.length;j++,k++)
+            ans[k]=input.charAt(i)+smallAns[j];
+                
+        }
+     
+            
+        
+		return ans;
+	}
+	
+}
