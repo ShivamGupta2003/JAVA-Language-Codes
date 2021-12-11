b9c859cad047c4aedf175f8b19844919f21a3103
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-11 12:44:43 +0530

    Update QuickSort.java

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -68,8 +68,30 @@ public class Solution {
         quickSort(input,pi+1,ei);
     }
     
-    
-    
-    
 	
+}
+
+//RUNNER CODE
+import java.util.Scanner;
+
+public class Runner {
+	
+	static Scanner s = new Scanner(System.in);
+	
+	public static int[] takeInput(){
+		int size = s.nextInt();
+		int[] input = new int[size];
+		for(int i = 0; i < size; i++){
+			input[i] = s.nextInt();
+		}
+		return input;
+	}
+	
+	public static void main(String[] args) {
+		int[] input = takeInput();
+		Solution.quickSort(input);
+		for(int i = 0; i < input.length; i++) {
+			System.out.print(input[i] + " ");
+		}
+	}
 }
