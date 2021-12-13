f2ffaa7bb75c2a9aa68863c1f77d97eadd885ac5
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-13 14:20:49 +0530

    Update Insertion_Sort.java

--- Arrays/Insertion_Sort.java
+++ Arrays/Insertion_Sort.java
@@ -1,3 +1,7 @@
+// QUESTION
+/*Provided with a random integer array/list(ARR) of size N, you have been required to sort this array using 'Insertion Sort'.*/
+
+// FUNCTIONAL CODE
 
 public class Solution {  
 
@@ -19,3 +23,56 @@ public class Solution {
     }
     }
 
+// MAIN CODE
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static int[] takeInput() throws IOException {
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] input = new int[size];
+
+        if (size == 0) {
+            return input;
+        }
+        
+        String[] strNums; 
+        strNums = br.readLine().split("\\s");
+        
+
+        for (int i = 0; i < size; ++i) {
+            input[i] = Integer.parseInt(strNums[i]);
+        }
+
+        return input;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while(t > 0) {
+
+            int[] input = takeInput();
+            Solution.insertionSort(input);
+            printArray(input);
+
+            t -= 1;
+        }
+    }
+}
+
+
+
+//Worst case time complexcity of Insertion sort is O(n^2)
