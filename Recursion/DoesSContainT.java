f1f7bff29d110ceec15cfbec80bb07220e3eecda
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-16 13:35:06 +0530

    Update DoesSContainT.java

--- Recursion/DoesSContainT.java
+++ Recursion/DoesSContainT.java
@@ -1,23 +1,52 @@
+// QUESTION
+
+/*Given two string s and t, write a function to check if s contains all characters of t (in the same order as they are in string t).
+Return true or false.
+Do it recursively.
+E.g. : s = “abchjsgsuohhdhyrikkknddg” contains all characters of t=”coding” in the same order. So function will return true.*/
+
+
+
+
+
+// FUNCTIONAL CODE
 
 public class Solution {
-	public static boolean checkSequence(String a, String b) {
 	
-        if(a.length()==0)
-        {
-            return false;
-        }
-        if(b.length()==0)
-        {
-            return true;
-        }
-        if(a.charAt(0)==b.charAt(0))
-        {
-            return checkSequence(a.substring(1),b.substring(1));
-        }
-        else
-        {
-            return checkSequence(a.substring(1),b);
-        }
+    
+    public static boolean checkSequence(String a, String b) {
+		return checkSequence(a,b,0,0);
+	}
+    
+     public static boolean checkSequence(String a, String b,int si,int l)
+     {
+         if(si==b.length())
+             return true;
+         
+    for(int i=l;i<a.length();i++)
+    {
+      if(b.charAt(si)==a.charAt(i))
+      {
+          return checkSequence(a,b,si+1,i+1);
+      }
+    }
+   
+    return false;
+}
+}
 
+
+
+// RUNNER CODE
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		String large = s.nextLine();
+		String small = s.nextLine();
+
+		System.out.println(Solution.checkSequence(large, small));
 	}
 }
