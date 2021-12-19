f4ca07b1cd47c58b24c58d6fe2a79b22b31d5003
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-19 15:13:12 +0530

    Create ReturnAllCode.java

---
+++ Recursion/ReturnAllCode.java
@@ -0,0 +1,60 @@
+// QUESTION
+Assume that the value of a = 1, b = 2, c = 3, ... , z = 26. You are given a numeric string S. Write a program to return the list of all possible codes that can be generated from the given string.
+Note : The order of codes are not important. And input string does not contain 0s.
+Input format :
+A numeric string
+Constraints :
+1 <= Length of String S <= 10
+  
+  
+//  FUNCTIONAL CODE
+  public class solution {
+    
+	public static char getChar(int n)
+    {
+        return (char)(96+n);
+    }
+    
+    
+	public static  String[] getCode(String input){
+		   
+        if(input.length()==0)
+        {
+            String ans[]={""};
+            return ans;
+        }
+       
+       int fdig=(input.charAt(0)-'0');
+       String sa1[]=getCode(input.substring(1));
+       char fchar=getChar(fdig);
+       
+       String sa2[]=new String[0];
+       char f2char=(char)(0); 
+       int f2dig=0;
+       
+        if(input.length()>=2)
+        {
+            f2dig=((input.charAt(0)-'0')*10)+(input.charAt(1)-'0');
+      
+            
+             if(f2dig>=10 && f2dig<=26)
+             {
+                 f2char=getChar(f2dig);
+                    sa2=getCode(input.substring(2));
+             }
+        }
+     
+        String ans[]=new String[sa1.length+sa2.length];
+            
+            int k=0;
+        
+        for(int i=0;i<sa1.length;i++,k++)
+        ans[k]=fchar+sa1[i];
+        
+        for(int i=0;i<sa2.length;i++,k++)
+        ans[k]=f2char+sa2[i];
+            
+        return ans;    
+	}
+
+}
