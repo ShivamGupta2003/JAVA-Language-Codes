22d8035e51f625e05370b814bb56b804beab6686
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-19 15:13:49 +0530

    Update ReturnAllCode.java

--- Recursion/ReturnAllCode.java
+++ Recursion/ReturnAllCode.java
@@ -1,10 +1,10 @@
 // QUESTION
-Assume that the value of a = 1, b = 2, c = 3, ... , z = 26. You are given a numeric string S. Write a program to return the list of all possible codes that can be generated from the given string.
-Note : The order of codes are not important. And input string does not contain 0s.
-Input format :
-A numeric string
-Constraints :
-1 <= Length of String S <= 10
+// Assume that the value of a = 1, b = 2, c = 3, ... , z = 26. You are given a numeric string S. Write a program to return the list of all possible codes that can be generated from the given string.
+// Note : The order of codes are not important. And input string does not contain 0s.
+// Input format :
+// A numeric string
+// Constraints :
+// 1 <= Length of String S <= 10
   
   
 //  FUNCTIONAL CODE
