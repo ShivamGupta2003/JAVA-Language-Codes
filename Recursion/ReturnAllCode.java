8b792e50fb3505e3b6012c2a7491412545d36a01
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-19 15:13:40 +0530

    Update ReturnAllCode.java

--- Recursion/ReturnAllCode.java
+++ Recursion/ReturnAllCode.java
@@ -58,3 +58,19 @@ Constraints :
 	}
 
 }
+
+// MAIN CODE
+import java.util.Scanner;
+
+public class runner {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String input = s.next();
+		String output[] = solution.getCode(input);
+		for(int i = 0; i < output.length; i++) {
+			System.out.println(output[i]);
+		}
+	}
+}
+
