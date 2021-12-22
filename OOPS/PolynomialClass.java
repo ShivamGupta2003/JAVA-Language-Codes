46f7fa88d608aebc1bc2198831baaca03ea27b2e
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-22 18:07:34 +0530

    Update PolynomialClass.java

--- OOPS/PolynomialClass.java
+++ OOPS/PolynomialClass.java
@@ -10,7 +10,7 @@ Multiplies two polynomials and returns a new polynomial which has the result.
 5. print -
 Prints all the terms (only terms with non zero coefficients are to be printed) in increasing order of degree.
 Print pattern for a single term : "x"
-And multiple terms should be printed separated by space. For more clarity, refer sample test cases.
+And multiple terms should be printed separated by space. 
 Note : Only keep those terms which have non - zero coefficients.*/
 
 
