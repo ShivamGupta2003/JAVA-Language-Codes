20142b89c505608f31df966e56c68246f8b15d7f
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-24 14:06:43 +0530

    Update CheckPermutation.java

--- Strings/CheckPermutation.java
+++ Strings/CheckPermutation.java
@@ -1,3 +1,6 @@
+// QUESTION
+
+// FUNCTIONAL CODE
 public class Solution {
 
 	public static boolean isPermutation(String str1, String str2) {
@@ -6,10 +9,7 @@ public class Solution {
             return false;
         
         int fre[] = new int[256];
-        
-      //  for(int i=0;i<256;i++){
-       //     fre[i]=0;
-     //   }
+   
         
         for(int i=0;i<str1.length();i++){
             char ch = str1.charAt(i);
