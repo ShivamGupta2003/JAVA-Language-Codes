9253b07496df58043a497b9698b1557f09e695a7
Shivamgupta-2003 <93479496+Shivamgupta-2003@users.noreply.github.com>
2021-12-26 21:46:06 +0530

    Update RectanguleOfStars.java

--- Patterns/RectanguleOfStars.java
+++ Patterns/RectanguleOfStars.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
-public class Solution {
-
-
-	public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
-        int n1=s.nextInt();
-        int n2=s.nextInt();
-        int i=1;
+public class Main {
+  public static void main(String[] args)
+  {
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    for (int i = 1; i <= n1; i++)
+    {
+      for (int j = 1; j <= n2; j++)
+      {
         
-            while(i<=n1)
-            {
-                int j=1;
-                while(j<=n2)
-                {
-                    
-                    System.out.print("*");
-                        j++;
-                }
-                System.out.println();
-                i++;
-            }
         
-		
-		
-		
-	}
-
+    if ( i == 1 || i == n1) 
+    System.out.print("*");
+        
+       
+    else if(j==1 || j==n2)
+    System.out.print("*");
+      
+      
+    else
+    System.out.print(" ");
+      
+      
+    }
+    System.out.println();
+  }
+  
+}
 }
