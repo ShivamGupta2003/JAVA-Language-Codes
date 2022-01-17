f89fc0afed57c99c5d46df13ff15a89c04700c1a
Shivam Gupta <93479496+Shivamgupta-2003@users.noreply.github.com>
2022-01-17 11:32:23 +0530

    Modified Code

--- Linked_Lists/24. Delete Alternate Nodes In Linked List.java
+++ Linked_Lists/24. Delete Alternate Nodes In Linked List.java
@@ -1,14 +1,13 @@
-                                                                      ||  QUESTION   ||       
-
+/*
 Given a Singly Linked List of integers, delete all the alternate nodes in the list.
+
 Example:
 List: 10 -> 20 -> 30 -> 40 -> 50 -> 60 -> null
 Alternate nodes will be:  20, 40, and 60.
-
 Hence after deleting, the list will be:
+
 Output: 10 -> 30 -> 50 -> null
-  
-  
+
 Note :
 The head of the list will remain the same. Don't need to print or return anything.
 
@@ -21,11 +20,11 @@ The only line of output will contain the updated list elements.
 Input Constraints:
 1 <= N <= 10 ^ 6.
 Where N is the size of the Singly Linked List
-
 Time Limit: 1 sec
 
 Sample Input 1:
 1 2 3 4 5 -1
+
 Sample Output 1:
 1 3 5
 
@@ -33,13 +32,17 @@ Explanation of Sample Input 1: 2, 4 are alternate nodes so we need to delete the
 
 Sample Input 2:
 10 20 30 40 50 60 70 -1
+
 Sample Output 2:
 10 30 50 70  
-                                 
-                                                        || FUNCTIONAL CODE ||   :- Iterative Approach
-  
+*/                                 
+                                                 
+
+
+//Solution Class that contains functions used for solving problem  
  public class Solution {
 
+	 //Function that we wished to create that deletes alternate Nodes Iteratively
     public static void deleteAlternateNodes(Node<Integer> head) {
         
         if(head.next==null || head.next.next==null)
@@ -71,29 +74,10 @@ Sample Output 2:
 	}
 }
 
-TIME COMPLEXCITY FOR THIS IS O(n)
-  
-  
-                                                        || FUNCTIONAL CODE ||   :- Recursive Approach
-  
-public class Solution {
-    public static void deleteAlternateNodes(Node<Integer> head) {
-        if(head==null || head.next==null )
-            return ;
-        deleteAlternateNodes(head.next.next) ;
-head.next=head.next.next;
-	}
-}
-  
-TIME COMPLEXCITY FOR THIS IS O(n)
+ 
+//Main Code:-
 
-	Note:-  THIS RECURSIVE APPROACH IS NOT USEFUL IN THIS QUESTION BECAUSE OF CONSRAINTS OF STACK OVERFLOW THERFORE FOR GREATER LENGTH OUR TEST CASE WILL FAILL
-  
-  
-                                                          || RUNNER CODE || 
-  
-  import java.util.Scanner;
-
+import java.util.Scanner;
 class Node<T> {
     public T data;
     public Node<T> next;
