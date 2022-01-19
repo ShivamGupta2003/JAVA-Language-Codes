790b31a7c8e99087a79fea93c16f00c868d8e94e
Shivam Gupta <93479496+Shivamgupta-2003@users.noreply.github.com>
2022-01-19 11:59:24 +0530

    Create 04. Queue Using Linked Lists.java

---
+++ Stacks and Queues/04. Queue Using Linked Lists.java
@@ -0,0 +1,102 @@
+//Question Create a Queue class and write code to create queue
+
+// Node Class For Creating Linked Lists
+public class Node<T> {
+	
+	T data;
+	Node<T> next;
+	
+	Node(T data){
+		this.data = data;
+		next = null;
+	}
+	
+}
+
+
+// Our Queue Class that we wished to create
+
+public class QueueUsingLL<T> {
+
+	private Node<T> front;
+	private Node<T> rear;
+	private int size;
+
+	public QueueUsingLL() {
+		front = null;
+		rear = null;
+		size = 0;
+	}
+	int size(){
+		return size;
+	}
+
+	boolean isEmpty(){
+		return size == 0;
+	}
+
+	T front() throws QueueEmptyException{
+		if(size == 0){
+			throw new QueueEmptyException();
+		}	
+		return front.data;
+	}
+
+	void enqueue(T element){
+		Node<T> newNode = new Node<>(element);
+		if(rear == null){
+			front = newNode;
+			rear = newNode;
+		}else{
+			rear.next = newNode;
+			rear = newNode;
+		}
+		size++;
+
+	}
+
+	T dequeue() throws QueueEmptyException{
+		if(size == 0){
+			throw new QueueEmptyException();
+		}	
+		
+		T temp = front.data;
+		front = front.next;
+		if(size == 1){
+			rear = null;
+		}
+		size--;
+		
+		return temp;
+	}
+
+
+}
+
+
+
+//Main runner code that helps us to use and check functions of our queue class
+
+public class QueueUse {
+
+	public static void main(String[] args) {
+
+		QueueUsingLL<Integer> queue = new QueueUsingLL<>();
+		for(int i = 1; i <= 5; i++){
+			queue.enqueue(i);
+
+		}
+		
+		
+		while(! queue.isEmpty()){
+			try {
+				System.out.println(queue.dequeue());
+			} catch (QueueEmptyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
