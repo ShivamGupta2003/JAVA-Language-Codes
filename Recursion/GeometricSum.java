b26a85b4a08b2900139605c80ba407901440b5a3
Shivam Gupta <93479496+Shivamgupta-2003@users.noreply.github.com>
2022-01-21 12:18:45 +0530

    Modify code

--- Recursion/GeometricSum.java
+++ Recursion/GeometricSum.java
@@ -1,4 +1,32 @@
+/*
+Given k, find the geometric sum i.e. 1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k) using recursion.
 
+Input format :
+Integer k
+
+Output format :
+Geometric sum (upto 5 decimal places)
+
+Constraints :
+0 <= k <= 1000
+
+Sample Input 1 :
+3
+
+Sample Output 1 :
+1.87500
+
+Sample Input 2 :
+4
+
+Sample Output 2 :
+1.93750
+*/
+
+
+
+
+// Solution that contains functions for solving problem 
 public class solution {
 		
         public static double power(double n,double x){
@@ -27,3 +55,19 @@ public class solution {
         
 	}
 }
+
+
+// Main code
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class runner {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int k = s.nextInt();
+		double ans = solution.findGeometricSum(k);
+		DecimalFormat dec = new DecimalFormat("#0.00000");
+		System.out.println(dec.format(ans));
+	}
+}
+
