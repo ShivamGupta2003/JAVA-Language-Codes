eb8ce310613ea98b232b09bbe96587f9e154a455
Shivam Gupta <93479496+Shivamgupta-2003@users.noreply.github.com>
2022-01-22 19:21:55 +0530

    Modify Code

--- Recursion/14. Sum Of Digits.java
+++ Recursion/14. Sum Of Digits.java
@@ -1,6 +1,31 @@
-//Write a recursive function that returns the sum of the digits of a given integer.
-  
-// FUNCTIONAL CODE
+/*
+Write a recursive function that returns the sum of the digits of a given integer.
+
+Input format :
+Integer N
+
+Output format :
+Sum of digits of N
+
+Constraints :
+0 <= N <= 10^9
+
+Sample Input 1 :
+12345
+
+Sample Output 1 :
+15
+
+Sample Input 2 :
+9
+
+Sample Output 2 :
+9
+
+*/
+
+
+// Solution Class that contains function for solving question
 public class solution {
 
 	public static int sumOfDigits(int input){
@@ -24,4 +49,3 @@ public class runner {
 	}
 }
 
-//         Time complexcity is O(logn) in which base of log is 10
