d60253ae88ffb4dc80bee040b7980e46cc4df0d5
Shivam Gupta <93479496+Shivamgupta-2003@users.noreply.github.com>
2022-01-30 22:49:31 +0530

    Create 02. Taking Input Level Wise.java

---
+++ Binary Trees/02. Taking Input Level Wise.java
@@ -0,0 +1,156 @@
+ class BinaryTreeNode<T> {
+	public BinaryTreeNode(T data) {
+		this.data = data;
+	}
+	public T data;
+	public BinaryTreeNode<T> left;
+	public BinaryTreeNode<T> right;
+}
+
+
+class Node<T> {
+	
+	T data;
+	Node<T> next;
+	
+	Node(T data){
+		this.data = data;
+		next = null;
+	}
+	
+}
+
+
+class QueueEmptyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7243921724361015813L;
+
+}
+
+
+ class QueueUsingLL<T> {
+
+	private Node<T> front;
+	private Node<T> rear;
+	private int size;
+
+	public QueueUsingLL() {
+		front = null;
+		rear = null;
+		size = 0;
+	}
+	int size(){
+		return size;
+	}
+
+	boolean isEmpty(){
+		return size == 0;
+	}
+
+	T front() throws QueueEmptyException{
+		if(size == 0){
+			throw new QueueEmptyException();
+		}	
+		return front.data;
+	}
+
+	void enqueue(T element){
+		Node<T> newNode = new Node<>(element);
+		if(rear == null){
+			front = newNode;
+			rear = newNode;
+		}else{
+			rear.next = newNode;
+			rear = newNode;
+		}
+		size++;
+
+	}
+
+	T dequeue() throws QueueEmptyException{
+		if(size == 0){
+			throw new QueueEmptyException();
+		}	
+		
+		T temp = front.data;
+		front = front.next;
+		if(size == 1){
+			rear = null;
+		}
+		size--;
+		
+		return temp;
+	}
+
+
+}
+
+
+import java.util.Scanner;
+
+public class BinaryTreeUse {
+
+	public static void printTree(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return;
+		}
+		String toBePrinted = root.data + "";
+		if (root.left != null) {
+			toBePrinted += "L:" + root.left.data + ",";
+		}
+		
+		if (root.right != null) {
+			toBePrinted += "R:" + root.right.data;
+		}
+		System.out.println(toBePrinted);
+		printTree(root.left);
+		printTree(root.right);
+	}
+	
+	
+	public static BinaryTreeNode<Integer> takeInputLevelWise() {
+		Scanner s = new Scanner(System.in);
+		QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+		System.out.println("Enter root data");
+		int rootData = s.nextInt();
+		if (rootData == -1) {
+			return null;
+		}
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> front;
+			try {
+				front = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			System.out.println("Enter left child of " + front.data);
+			int leftChild = s.nextInt();
+			if (leftChild != -1) {
+				BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(leftChild);
+				pendingNodes.enqueue(child);
+				front.left = child;
+			}
+			
+			System.out.println("Enter right child of " + front.data);
+			int rightChild = s.nextInt();
+			if (rightChild != -1) {
+				BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(rightChild);
+				pendingNodes.enqueue(child);
+				front.right = child;
+			}
+		}
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInputLevelWise();
+		printTree(root);
+
+	}
+}
