639728430307ac19fdf4adcc339ffe26b53ab132
Shivam Gupta <93479496+Shivamgupta-2003@users.noreply.github.com>
2022-01-31 22:01:08 +0530

    Create 03. Print Tree Level Wise.java

---
+++ Binary Trees/03. Print Tree Level Wise.java
@@ -0,0 +1,259 @@
+/*
+
+For a given a Binary Tree of type integer, print the complete information of every node, when traversed in a level-order fashion.
+To print the information of a node with data D, you need to follow the exact format :
+           D:L:X,R:Y
+Where D is the data of a node present in the binary tree. 
+X and Y are the values of the left(L) and right(R) child of the node.
+Print -1 if the child doesn't exist.
+
+
+For the above depicted Binary Tree, the level order travel will be printed as followed:
+1:L:2,R:3
+2:L:4:,R:-1
+3:L:5:,R:6
+4:L:-1:,R:7
+5:L:-1:,R:-1    
+6:L:-1:,R:-1
+7:L:-1:,R:-1
+
+Note: There is no space in between while printing the information for each node.
+
+Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+
+Output Format:
+Information of all the nodes in the Binary Tree will be printed on a different line where each node will follow a format of D:L:X,R:Y, without any spaces in between.
+
+Constraints:
+1 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+
+Sample Output 1:
+8:L:3,R:10
+3:L:1,R:6
+10:L:-1,R:14
+1:L:-1,R:-1
+6:L:4,R:7
+14:L:13,R:-1
+4:L:-1,R:-1
+7:L:-1,R:-1
+13:L:-1,R:-1
+
+Sample Input 2:
+1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
+ 
+Sample Output 2:
+1:L:2,R:3
+2:L:4,R:5
+3:L:6,R:7
+4:L:-1,R:-1
+5:L:-1,R:-1
+6:L:-1,R:-1
+7:L:-1,R:-1
+
+*/
+
+
+
+// SOLUTION CLASS THAT CONTAINS FUNCTION TO SOLVE PROBLEM
+
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+public class Solution {
+
+	public static void printLevelWise(BinaryTreeNode<Integer> root) {
+	
+		Queue<BinaryTreeNode<Integer>> pendingNodes = new LinkedList<>();
+		pendingNodes.add(root);
+        pendingNodes.add(null);
+
+		while(!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> frontNode = pendingNodes.poll();
+
+	        
+            if (frontNode == null) {
+	           
+
+	        if (!pendingNodes.isEmpty()) 
+	                pendingNodes.add(null);
+            } 
+            
+            else {
+	        	System.out.print(frontNode.data + ":L:");
+
+	            if (frontNode.left != null) {
+	                pendingNodes.add(frontNode.left);
+                    System.out.print(frontNode.left.data+",R:");
+	            } 
+                
+                else if(frontNode.left == null)
+                     System.out.print("-1,R:");
+
+	            if (frontNode.right != null) {
+                     pendingNodes.add(frontNode.right);
+	                System.out.println(frontNode.right.data);
+	            } 
+                
+                else if(frontNode.right == null) 
+                    System.out.println("-1");
+	        }
+		}
+		
+	}
+}
+
+
+
+//  MAIN CLASS
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class QueueEmptyException extends Exception {
+
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+public class Runner {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+		QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); 
+		int start = 0;
+
+		String[] nodeDatas = br.readLine().trim().split(" ");
+
+		if (nodeDatas.length == 1) {
+			return null;
+		}
+
+		int rootData = Integer.parseInt(nodeDatas[start]);
+		start += 1;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+
+			int leftChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+
+			int rightChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+
+		return root;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		
+		Solution.printLevelWise(root);
+	}
+}
