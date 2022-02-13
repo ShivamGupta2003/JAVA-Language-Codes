488a8231e87d85e94bb1088124cb4ee2e849b08f
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-02-13 19:59:28 +0530

    Create 06. Find Path in BST.java

---
+++ Binary Search Trees/06. Find Path in BST.java
@@ -0,0 +1,211 @@
+/*
+
+Given a BST and an integer k. Find and return the path from the node with data k and root (if a node with data k is present in given BST) in a list. Return empty list otherwise.
+
+Note: Assume that BST contains all unique elements.
+
+Input Format :
+The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. If any node does not have left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.   
+The following line of input contains an integer, that denotes the value of k.
+
+Output Format :
+The first line and only line of output prints the data of the nodes in the path from node k to root. The data of the nodes is separated by single space.
+
+Constraints:
+Time Limit: 1 second   
+
+Sample Input 1:
+8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
+2
+
+Sample Output 1:
+2 5 8
+
+*/
+
+
+// SOLUTION CLASS THAT CONTAINS FUNCTIONS TO SOLVE PROIBELM
+
+import java.util.ArrayList;
+
+public class Solution {
+
+
+	public static ArrayList<Integer> getPath(BinaryTreeNode<Integer> root, int data){
+	   
+       if(root==null) {
+			return new ArrayList<Integer>();
+		}
+		
+        if( root.data == data ) {
+			ArrayList<Integer> arrayList = new ArrayList<>();
+			arrayList.add(data);
+			return arrayList;
+		}
+		
+		if(data < root.data) {
+			
+			ArrayList<Integer> leftArrayList = getPath(root.left, data);
+			if(leftArrayList.size()!=0) {
+				leftArrayList.add(root.data);
+			}
+			return leftArrayList;
+			
+		}
+		
+        if( data > root.data ) {
+			
+			ArrayList<Integer> rightArrayList = getPath(root.right, data);
+			if(rightArrayList.size()!=0) {
+				rightArrayList.add(root.data);
+			}
+			return rightArrayList;
+		}
+		return new ArrayList<Integer>();
+		
+		
+		
+	}
+
+}
+		
+		
+	
+
+
+
+// MAIN CLASS
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class QueueEmptyException extends Exception {
+}
+
+class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+
+		Node(T data) {
+			this.data = data;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException {
+		if (size == 0) {
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+	public void enqueue(T element) {
+		Node<T> newNode = new Node<T>(element);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException {
+		if (head == null) {
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if (head == tail) {
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+	}
+}
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	public static BinaryTreeNode<Integer> takeInput() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int rootData = Integer.parseInt(st.nextToken());
+		if (rootData == -1) {
+			return null;
+		}
+		QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>();
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			int leftChildData = Integer.parseInt(st.nextToken());
+			if (leftChildData != -1) {
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+			int rightChildData = Integer.parseInt(st.nextToken());
+			if (rightChildData != -1) {
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		int k = Integer.parseInt(br.readLine());
+		ArrayList<Integer> output = Solution.getPath(root, k);
+		if(output != null) {
+			for(int i : output) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
