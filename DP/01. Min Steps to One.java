018333bd1e5183e28a4e000c46bbde53f33c9d4b
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-02-22 18:55:04 +0530

    Dp added

--- DP/01. Min Steps to One.java
+++ DP/01. Min Steps to One.java
@@ -44,6 +44,10 @@ Step 3 : n = 2 / 2 = 1
 
 // SOLUTION CLASS THAT CONTAINS FUNCTIONS TO SOLVE OUR PRBOLEM
 
+
+
+// RECURSIVE SOLUTION 
+
 public class Solution {
 
 	public static int countMinStepsToOne(int n) {
@@ -83,4 +87,32 @@ public class Solution {
 
 
 
+// DP SOLUTION
 
+	public static int countMinStepsToOne(int n) {
+		int storage[]=new int[n+1];
+        
+        for(int i=2;i<=n;i++)
+        {
+            int min=storage[i-1];
+            
+            if(i%3==0)
+                if(min>storage[i/3])
+                    min=storage[i/3];
+            
+            
+             if(i%2==0)
+                if(min>storage[i/2])
+                    min=storage[i/2];
+            
+            storage[i]=1+min;
+        }
+         return storage[n];   
+        }
+        
+        
+      
+	}
+    
+    
+  
