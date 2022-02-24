ec071983b2503870225f0119d9d4128265106e38
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-02-24 22:06:42 +0530

    Update 02. Staircase.java

--- DP/02. Staircase.java
+++ DP/02. Staircase.java
@@ -77,6 +77,18 @@ public class Solution {
     
     
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+            int n = Integer.parseInt(br.readLine().trim());
+            System.out.println(Solution.staircase(n));
+    }
+}
 
 
