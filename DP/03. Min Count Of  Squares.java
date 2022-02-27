c8052ba820d8c9b9e2b4f9011e3081147eeddc0a
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-02-27 22:03:58 +0530

    Update and rename 03. Min Count Of  Squares to 03. Min Count Of  Squares.java

--- DP/03. Min Count Of  Squares.java
+++ DP/03. Min Count Of  Squares.java
@@ -29,3 +29,46 @@ Sample Input 2 :
 9
 Sample Output 2 :
 1
+*/
+
+public class Solution {
+
+	public static int minCount(int n) {
+
+    	int[] storage  = new int[n+1];
+		storage[0]=0;
+		storage[1] =1;
+		for(int i=2;i<=n;i++) {
+			int min = Integer.MAX_VALUE;
+			
+            for(int j=1;j<=Math.sqrt(i);j++) {
+				int count = storage[i-(j*j)];
+				if(min > count) {
+					min = count;
+				}
+				
+			}
+			storage[i] = min + 1;
+			
+		}
+		return storage[n];
+	}
+}
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+// Main CLASS
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+            int n = Integer.parseInt(br.readLine().trim());
+            System.out.println(Solution.minCount(n));
+    }
+}
