d5fed5419cfe59ab882f0cc74622553f956f58d5
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-02-28 22:58:12 +0530

    Modified Code

--- DP/05. No. Of Balanced BTs using DP.java
+++ DP/05. No. Of Balanced BTs using DP.java
@@ -51,3 +51,22 @@ public class Solution {
 		return storage[h]%mod;
 	}
 }
+
+
+// Main Class
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+
+        int n = Integer.parseInt(br.readLine().trim());
+        System.out.print(Solution.balancedBTs(n));
+
+
+    }
+}
